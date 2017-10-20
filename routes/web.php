3e04f73b33612929ecf92b31d9307cf654132dce
Stefan <stefanantic7@gmail.com>
2017-10-21 01:56:07 +0200

    initial

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//route for verification
+Route::get("/bot", "MainController@receive")->middleware("verify");
+
+//where Facebook sends messages to. No need to attach the middleware to this because the verification is via GET
+Route::post("/bot", "MainController@receive");
