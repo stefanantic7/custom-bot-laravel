7d607276f1f8645802c2cb387d8e0e1dbd27abe2
Stefan <stefanantic7@gmail.com>
2017-10-21 03:07:41 +0200

    initial

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -15,7 +15,6 @@ class MainController extends Controller
         $received = $data["entry"][0]["messaging"][0]["message"]["text"];
         if(is_string($received)){
             $this->sendTextMessage($id, $received);
-
         }
     }
 
@@ -29,7 +28,7 @@ class MainController extends Controller
                 "text" => $messageText
             ]
         ];
-        $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=' . 'EAAWc81PlKzgBANgu2jib02nbhtYMQFWZBcAxknNllRhLuj35ZCjucveKRIhLetCRCqbECwoB8c1ts30js7F6CZCumR1rkXpWdb7v2DTIvscZANX3pJpq0v5ZA631fuvBp1iN3vhUyGlC2bW7G0wRcOMq34Tgh8KcGS0jwq6e9LwZDZD');
+        $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=' . 'EAAWc81PlKzgBABXSnZCYZAFYsZA3CqBdT3isv0oUZCGQ9BMuZAc8cVMEtGRaZCUNUb1zV6r0uasKu8s1mpK5JiM8Ogxr2Vv3YvBthW8YX70Iwr4OZCivwOlUAH5enayHnbVZCCintx7GrgEXqOE5W9VVPaLLy7qtjM6i2gN3dDijuAZDZD');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
