e810f3e162ca0090ab16e9b2aea06f4db06e5ecc
Stefan <stefanantic7@gmail.com>
2017-10-21 02:47:11 +0200

    initial

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -11,8 +11,8 @@ class MainController extends Controller
         $data = $request->all();
         //get the user’s id
         $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
-
-        $this->sendTextMessage($id, $data["entry"][0]["messaging"][0]["message"]["text"]);
+//        $data["entry"][0]["messaging"][0]["message"]["text"]
+        $this->sendTextMessage($id, "hello");
     }
 
     private function sendTextMessage($recipientId, $messageText)
