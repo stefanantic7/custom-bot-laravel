bade568533855f1a65e451c8b769a19db8007e67
Stefan <stefanantic7@gmail.com>
2017-10-22 00:07:33 +0200

    Function for import rules from json file

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,29 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('json:import', function () {
+    $content = Storage::get('rules.json');
+    $rules_json = json_decode($content)->rules;
+    foreach ($rules_json as $rule_json){
+        $rule = new \App\Rule();
+        $conclusion = new \App\Statement();
+
+        $conclusion->text = $rule_json->conclusion;
+        $conclusion->conclusion_for_rule = $rule->id;
+
+        foreach ($rule_json->conditions as $condition_json) {
+            $condition = new \App\Statement();
+
+            $condition->text = $condition_json;
+            $condition->condition_for_rule = $rule->id;
+
+            $condition->save();
+        }
+
+
+        $conclusion->save();
+        $rule->save();
+    }
+
+})->describe('Import rules from json file');
