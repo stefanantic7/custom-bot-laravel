e1f86b4621fa74acbe7377e4a554e4a0c8b1bd01
Stefan <stefanantic7@gmail.com>
2017-10-21 02:25:47 +0200

    initial

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,7 +9,8 @@ class MainController extends Controller
     public function receive(Request $request)
     {
         $data = $request->all();
-        dd($data);
+        \Log::info($data);
+
         //get the user’s id
         $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
         $this->sendTextMessage($id, $id = $data["entry"][0]["messaging"][0]["message"]);
